29a9125851dd1c61e9f798812125231c5f84e138
PGimenez <gimenezfebrer@gmail.com>
2024-05-10 16:43:49 +0200

    simplify code

--- app.jl
+++ app.jl
@@ -1,48 +1,32 @@
-#todo when modifying a number from the browser in an input, its type is changed to string int the backdend. perhaps because the number is stored in a dictionary without strict typing
 using GenieFramework, PlotlyBase, DataFrames, JLD2, BSON
 using StippleLatex, Latexify, StippleDownloads
 include("mppt.jl")
+include("utils.jl")
 
 Stipple.Layout.add_script("https://cdn.tailwindcss.com")
 Stipple.Layout.add_script("https://cdn.jsdelivr.net/npm/@joint/core@4.0.1/dist/joint.js")
 
 @load "S.jld2" sol_stored
 
-to_float(x) = typeof(x) == String ? parse(Float64, x) : x
+
 @app begin
     @in var=0
     @in components = component_config
     @out x = collect(1:5000)*1.0
     @out y = zeros(1:500)*1.0
     @out trace=[scatter()]
-    @out layout = PlotlyBase.Layout(
-                                    Dict{Symbol,Any}(:paper_bgcolor => "rgb(226, 232, 240)",
-                                                     :plot_bgcolor => "rgb(226, 232, 240)");
-                                    xaxis_title="t (s)",
-                                    legend=attr( x=1, y=1.02, yanchor="bottom", xanchor="right", orientation="h",),
-                                    xaxis=attr(gridcolor="red", gridwidth=4),
-                                    yaxis=attr(gridcolor="red"),
-                                    margin=Dict(:l => 10, :r => 10, :b => 10, :t => 10),
-
-                                   )
     @in selected_comp = :Battery
     @out uknowns = unknowns_list
     @in selected_unknown = ["Battery₊v(t)", "MPPT₊in₊i(t)"]
-    @in txt = ""
     @in simulate = false
     @in T = 1.5
-    @private S = sol_stored
+    @out S = sol_stored
     @in download = false
     @onbutton simulate begin
         @info "Running simulation..."
-        param_values = Dict( eval(Meta.parse(c["name"]*"."*string(p.first))) => to_float(p.second) for c in values(components) for p in c["parameters"])
-        state_values = Dict( eval(Meta.parse(c["name"]*"."*string(p.first))) => to_float(p.second) for c in values(components) for p in c["states"])
-        u0 = [
-              Battery.v_s => 0.1,
-              Battery.v_f => 0.1,
-              Battery.v_soc => 0.3
-             ]
-        prob = ODEProblem(sys, u0, (0.0,T*1000), param_values)
+        param_values = symbolic_dict(components, "parameters")
+        state_values = symbolic_dict(components, "states")
+        prob = ODEProblem(sys, state_values, (0.0,T*1000), param_values)
         sol = solve(prob,Rosenbrock23());
         sol_matrix = hcat(sol.u...)
         S = DataFrame(sol_matrix', unknowns_list)
@@ -50,8 +34,6 @@ to_float(x) = typeof(x) == String ? parse(Float64, x) : x
         notify(__model__.selected_unknown)
         notify(__model__, "Simulation complete")
         @info "Simulation completed"
-        # solution = S # calling @save on S causes problems, need to make a non-observable copy
-        # BSON.@save "S.bson" solution
     end
     @onchange isready,selected_unknown begin
         trace[!] = []
@@ -60,37 +42,8 @@ to_float(x) = typeof(x) == String ? parse(Float64, x) : x
         end
         trace = copy(trace)
     end
-
-    @event uploaded begin
-        notify(__model__, "Upload finished")
-        notify(__model__.selected_unknown)
-        @info "uploaded"
-    end
-    @onchange fileuploads begin
-        if ! isempty(fileuploads)
-            @info "File was uploaded: " fileuploads
-            BSON.@load fileuploads["path"]  sol_stored
-            S = sol_stored
-            rm(fileuploads["path"])
-
-            fileuploads = Dict{AbstractString,AbstractString}()
-        end
-    end
-    @event download begin
-        try
-            solution = S
-            io = IOBuffer()
-            BSON.@save io solution
-            seekstart(io)
-            download_binary(__model__, take!(io), "simulation.bson")
-        catch ex
-            println("Error during download: ", ex)
-        end
-    end
 end
 
 @page("/", "app.jl.html")
 
-include("components/utils.jl")
 load_component("component-props")
-register_normal_element("component__props", context = @__MODULE__)
--- utils.jl
+++ utils.jl
@@ -1,7 +1,14 @@
 import GenieFramework.Genie.Renderer.Html: normal_element, register_normal_element
+
+to_float(x) = typeof(x) == String ? parse(Float64, x) : x
+
+
+symbolic_dict(components, key) = Dict( eval(Meta.parse(c["name"]*"."*string(p.first))) => to_float(p.second) for c in values(components) for p in c[key])
+
 function load_component(name)
     component_code() = [script(read("components/$name.js", String))]
     @deps Main_ReactiveModel component_code
     #= Stipple.register_components(Main_ReactiveModel, name, legacy = true) =#
     register_normal_element("component__props", context = @__MODULE__)
 end
+
